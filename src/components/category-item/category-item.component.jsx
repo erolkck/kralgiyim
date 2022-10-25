f48b481d6795bb230368f8d0268a8d1ecc7446aa
erolkck <erolkucuk1907@gmail.com>
2022-10-26 02:47:28 +0300

    first commit

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import './category-item.styles.scss'
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category
+  return (
+    <div className='kategori-konteynırı'>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      {/* <img src="" alt="" /> */}
+      <div className='kategori-gövde-konteynırı'>
+        <h2>{title}</h2>
+        <p>SHOP NOW</p>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryItem
